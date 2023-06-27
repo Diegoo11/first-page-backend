7952ad96c7c3a32d074a5881d87fc9153a92a50e
Diegoo11 <ynoacamino@gmail.com>
2023-06-26 19:18:19 -0500

    create app for apollo client

---
+++ app.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/extensions */
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import dotenv from 'dotenv';
+import typeDefs from './typeDefs/typeDefs.js';
+import resolvers from './resolvers/resolvers.js';
+import './db/db.js';
+
+dotenv.config();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: process.env.URL },
+});
+
+console.log(`Server ready at ${url}`);
