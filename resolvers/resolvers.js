9f88518bbb5903e94bba8cd483e1fe1df776e84e
Diegoo11 <ynoacamino@gmail.com>
2023-06-27 19:41:32 -0500

    new querys and mutations

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,6 +1,16 @@
+/* eslint-disable import/extensions */
+import editImage from './mutation/editImage.js';
+import createImage from './mutation/createImage.js';
+import findSingleImage from './queary/findSingleImage.js';
+
 const resolvers = {
   Query: {
     testPerson: () => 'test',
+    findSingleImage,
+  },
+  Mutation: {
+    editImage,
+    createImage,
   },
 };
 
