94bbb4f905bf20a9ede882e9f03a66febab980ee
Diegoo11 <ynoacamino@gmail.com>
2023-06-27 20:59:08 -0500

    typeDefs of Image Product User and querys

--- typeDefs/typeDefs.js
+++ typeDefs/typeDefs.js
@@ -1,10 +1,67 @@
 const typeDefs = `#graphql
-  type Person {
+  type Image {
+    src: String!
+    srcMobile: String
+    alt: String!
+    mod: String!
+    mId: String!
+    link: String
+    id: ID!
+  }
+
+  type Product {
+    sku: String!
+    cost: Int!
+    stock: Boolean!
     name: String!
+    description: String!
+    brand: String!
+    tags: [String!]
+    id: ID!
+  }
+
+  type Text {
+    name: String!
+    description: String!
+    link: String
+    mod: String!
+    mId: String!
+    id: ID!
+  }
+  
+  type User {
+    username: String!
+    password: String!
+    cart: [Product]!
+    id: ID!
   }
 
   type Query {
     testPerson: String!
+
+    findSingleImage(
+      id: ID!
+    ): Image
+  }
+
+  type Mutation {
+    createImage(
+      src: String!
+      srcMobile: String
+      alt: String!
+      mod: String!
+      mId: String!
+      link: String
+    ): Image
+
+    editImage(
+      src: String
+      srcMobile: String
+      alt: String
+      mod: String
+      mId: String
+      link: String
+    ): Image
   }
 `;
 
