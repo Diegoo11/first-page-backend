0abfbe859a1fd9c61fd4586a1ab60a9ee78dc7f4
Diegoo11 <ynoacamino@gmail.com>
2023-06-28 22:26:40 -0500

    mutation loginAdmin added

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -3,6 +3,7 @@ import editImage from './mutation/editImage.js';
 import createImage from './mutation/createImage.js';
 import findSingleImage from './queary/findSingleImage.js';
 import findImagesForModule from './queary/findImagesForModule.js';
+import loginAdmin from './mutation/loginAdmin.js';
 
 const resolvers = {
   Query: {
@@ -11,6 +12,7 @@ const resolvers = {
     findImagesForModule,
   },
   Mutation: {
+    loginAdmin,
     editImage,
     createImage,
   },
