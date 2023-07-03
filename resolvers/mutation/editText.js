0b762e0185d00a6cdbcf3d39c6440803033d3beb
Diegoo11 <ynoacamino@gmail.com>
2023-07-02 22:19:07 -0500

    new mutation for edir text

---
+++ resolvers/mutation/editText.js
@@ -0,0 +1,25 @@
+import { GraphQLError } from 'graphql';
+import Text from '../../models/Text.js';
+
+const editText = async (root, args) => {
+  console.log(args);
+  const text = await Text.findById(args.id);
+  const { name, description, link } = args;
+  text.name = name;
+  text.description = description;
+  text.link = link;
+
+  try {
+    await text.save();
+  } catch (err) {
+    throw new GraphQLError(err.message, {
+      extensions: {
+        code: 'SERVER_DATABASE_ERROR',
+        invalidArgs: args,
+      },
+    });
+  }
+  return text;
+};
+
+export default editText;
