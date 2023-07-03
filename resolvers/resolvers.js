6dc130f9080da7d13b4dac7e377b9197b3e0466d
Diegoo11 <ynoacamino@gmail.com>
2023-07-02 22:15:56 -0500

    new resolvers: createText, findForModule, editText

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -4,17 +4,23 @@ import createImage from './mutation/createImage.js';
 import findSingleImage from './queary/findSingleImage.js';
 import findImagesForModule from './queary/findImagesForModule.js';
 import loginAdmin from './mutation/loginAdmin.js';
+import createText from './mutation/createText.js';
+import findForModule from './queary/findForModule.js';
+import editText from './mutation/editText.js';
 
 const resolvers = {
   Query: {
     testPerson: () => 'test',
     findSingleImage,
     findImagesForModule,
+    findForModule,
   },
   Mutation: {
     loginAdmin,
     editImage,
+    editText,
     createImage,
+    createText,
   },
 };
 
