d3688d4378d92e904014f910e2b337cea2b948fe
Diegoo11 <ynoacamino@gmail.com>
2023-07-25 20:26:45 -0500

    add resolver that edits Item

---
+++ resolvers/mutation/editItem.js
@@ -0,0 +1,38 @@
+import { GraphQLError } from 'graphql';
+import Item from '../../models/Item.js';
+
+const editItem = async (root, args) => {
+  const {
+    id, name, cost, stock, description, brand, tags, src,
+  } = args;
+  let item;
+
+  try {
+    item = await Item.findById(id);
+  } catch (err) {
+    throw new GraphQLError(err.message, {
+      extensions: {
+        code: 'USER_INPUT_ERROR',
+        invalidArgs: id,
+      },
+    });
+  }
+
+  item.name = name;
+  item.cost = cost;
+  item.src = src;
+  item.stock = stock;
+  item.description = description;
+  item.brand = brand;
+  item.tags = tags;
+
+  try {
+    await item.save();
+  } catch (err) {
+    throw new GraphQLError(err.message);
+  }
+
+  return item;
+};
+
+export default editItem;
