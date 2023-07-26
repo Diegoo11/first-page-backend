c9d09d286ee55188db7abb10b40fdbff262b2205
Diegoo11 <ynoacamino@gmail.com>
2023-07-25 20:21:28 -0500

    add resolver than create a Item

---
+++ resolvers/mutation/createItem.js
@@ -0,0 +1,19 @@
+import { GraphQLError } from 'graphql';
+import Item from '../../models/Item.js';
+
+const createItem = async (root, args) => {
+  const item = new Item({ ...args });
+  try {
+    await item.save();
+  } catch (err) {
+    throw new GraphQLError(err.message, {
+      extensions: {
+        code: 'USER_INPUT_ERROR',
+        invalidArgs: args,
+      },
+    });
+  }
+  return item;
+};
+
+export default createItem;
