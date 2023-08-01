d6c534666d1e1afc879b0f70f58d653257a50259
Diegoo11 <ynoacamino@gmail.com>
2023-07-31 20:44:48 -0500

    fix the "new item not found" error

--- resolvers/mutation/createItem.js
+++ resolvers/mutation/createItem.js
@@ -3,6 +3,7 @@ import Item from '../../models/Item.js';
 
 const createItem = async (root, args) => {
   const item = new Item({ ...args });
+  item.tags = ['', ''];
   try {
     await item.save();
   } catch (err) {
